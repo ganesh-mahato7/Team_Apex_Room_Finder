d97af03a2f904d1650a2079ce29afb8cc4424fd3
Kumar-Shah10 <shahkumar2061@gmail.com>
2026-05-21 17:58:10 +0545

    Initialize FrontEnd

--- Backend/Route/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addUser, login } = require("../controller/userController");
-
-const router = express.Router();
-
-router.post("/create", addUser);
-router.post("/login", login);
-
-module.exports = router;
