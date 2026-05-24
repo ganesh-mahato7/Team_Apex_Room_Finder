98e8a6e6807e94fadbf72bc451d24016efa5f4e5
ganesh-mahato7 <ganeshmahato1412@gmail.com>
2026-05-24 21:02:04 +0545

    Updated app routing

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn    from "./pages/SignIn";
+import Register  from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword  from "./pages/ResetPassword";
 import AddProperty from "./pages/AddProperty";
+
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signin"   element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password"       element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/add-property" element={<AddProperty />} />
+        
+
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
